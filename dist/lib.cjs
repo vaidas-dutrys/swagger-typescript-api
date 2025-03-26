e15f419ec6ffc51b3810aa881de5652607fc2550
vaidas-dutrys <vaidas@dutrys.com>
2025-03-26 14:10:38 +0200

    update

---
+++ dist/lib.cjs
@@ -0,0 +1,2 @@
+'use strict';var chunk7ALSDI3S_cjs=require('./chunk-7ALSDI3S.cjs');Object.defineProperty(exports,"constants",{enumerable:true,get:function(){return chunk7ALSDI3S_cjs.c}});Object.defineProperty(exports,"generateApi",{enumerable:true,get:function(){return chunk7ALSDI3S_cjs.g}});Object.defineProperty(exports,"generateTemplates",{enumerable:true,get:function(){return chunk7ALSDI3S_cjs.f}});//# sourceMappingURL=lib.cjs.map
+//# sourceMappingURL=lib.cjs.map
---
+++ dist/lib.js
@@ -0,0 +1,2 @@
+export{c as constants,g as generateApi,f as generateTemplates}from'./chunk-U3YR4GKS.js';//# sourceMappingURL=lib.js.map
+//# sourceMappingURL=lib.js.map
